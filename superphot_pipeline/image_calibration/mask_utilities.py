8a4e7cc3aa0ea77319b367d80209af839ab0cd0c
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2019-02-07 09:57:45 -0600

    Moved hat mask utilities to SuperPhot.

--- superphot_pipeline/image_calibration/mask_utilities.py
+++ superphot_pipeline/image_calibration/mask_utilities.py
@@ -7,97 +7,14 @@ Attributes:
         corresponding bitmasks.
 """
 
-from ctypes import cdll, c_long, c_byte, c_char_p
-from ctypes.util import find_library
-
 import numpy
+
+from superphot.hat_masks import parse_hat_mask, mask_flags
 
 from superphot_pipeline.image_utilities import read_image_components
 from superphot_pipeline.pipeline_exceptions import ImageMismatchError
 
 git_id = '$Id$'
-
-mask_flags = dict()
-
-def initialize_library():
-    """Prepare the superphotio library for use."""
-
-    library_fname = find_library('superphotio')
-    if library_fname is None:
-        raise OSError("Unable to find SuperPhot's io library.")
-    library = cdll.LoadLibrary(library_fname)
-
-    library.parse_hat_mask.argtypes = [
-        c_char_p,
-        c_long,
-        c_long,
-        numpy.ctypeslib.ndpointer(dtype=c_byte,
-                                  ndim=1,
-                                  flags='C_CONTIGUOUS')
-    ]
-    library.parse_hat_mask.restype = None
-
-    for flag in ['OK',
-                 'CLEAR',
-                 'FAULT',
-                 'HOT',
-                 'COSMIC',
-                 'OUTER',
-                 'OVERSATURATED',
-                 'LEAKED',
-                 'SATURATED',
-                 'INTERPOLATED',
-                 'BAD',
-                 'ALL',
-                 'NAN']:
-        mask_flags[flag] = c_byte.in_dll(library, 'MASK_' + flag).value
-
-    return library
-
-superphotio_lib = initialize_library()
-
-def parse_hat_mask(header):
-    """
-    Extract the HAT-style mask contained in header.
-
-    Args:
-        header:    The header of the image whose mask to parse.
-
-    Returns:
-        numpy.array(dtype=uint8):
-            array with exactly the same resolution as the input image containing
-            a bit-field for each pixel indicating any bad-pixel flags raised per
-            the header.
-
-    Examples:
-
-        >>> from astropy.io import fits
-        >>>
-        >>> with fits.open('/Users/kpenev/tmp/1-447491_4.fits.fz',
-        >>>                mode='readonly') as f:
-        >>>     image_mask = parse_hat_mask(f[1].header)
-        >>>
-        >>>     flag_name = 'OVERSATURATED'
-        >>>
-        >>>     matched = numpy.bitwise_and(image_mask,
-        >>>                                 mask_flags[flag_name]).astype(bool)
-        >>>
-        >>>     #Print number of pixels for which the OVERSATURATED flag is raised
-        >>>     print(flag_name + ': ' + repr(matched.sum()))
-        >>>
-        >>>     #Output x, y, flux for the pixels flagged as OVERSATURATED
-        >>>     for y, x in zip(*numpy.nonzero(matched)):
-        >>>         print('%4d %4d %15d' % (x, y, f[1].data[y, x]))
-    """
-
-    mask_string = ''.join((c[1] + ' ') if c[0] == 'MASKINFO' else ''
-                          for c in header.items()).encode('ascii')
-    mask = numpy.zeros((header['NAXIS2'], header['NAXIS1']), dtype='int8')
-    superphotio_lib.parse_hat_mask(mask_string,
-                                   header['NAXIS1'],
-                                   header['NAXIS2'],
-                                   mask.ravel())
-    return mask
 
 def combine_masks(mask_filenames):
     r"""
