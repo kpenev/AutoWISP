e8ec16e1f2a31488f8c7bdb7e8470eeaa03ee250
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2019-05-10 13:46:18 -0500

    Added script to parsers of fitting terms exressions.

---
+++ scripts/generate_fit_term_parser.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function antlr4()
+{
+    java \
+        -Xmx500M \
+        -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" \
+        org.antlr.v4.Tool \
+        "$@"
+}
+
+#ANTLR set-up
+export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
+
+THIS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DESTINATION=${THIS_PATH}/../superphot_pipeline/fit_terms
+
+rm -f tmp/*
+antlr4 ${THIS_PATH}/FitTermsLexer.g4 ${THIS_PATH}/FitTermsParser.g4 -Dlanguage=Python3 -visitor -o tmp
+
+mkdir -p ${DESTINATION}
+
+for f in tmp/FitTerms*.py; do
+    FNAME=$(basename $f)
+    (echo "# pylint: skip-file"; cat $f) > ${DESTINATION}/${FNAME}
+done
