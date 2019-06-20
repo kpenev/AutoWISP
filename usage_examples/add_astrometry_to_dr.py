341cb85647c6d39fb2ed9cb96ff5da2271255489
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2019-06-20 17:37:42 -0500

    Added example of how to derive smooth srcextract PSF map.

---
+++ usage_examples/add_astrometry_to_dr.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from os.path import dirname, join as join_paths
+
+from superphot_pipeline import DataReductionFile
+
+if __name__ == '__main__':
+    data_dir = join_paths(dirname(__file__), 'test_data')
+
+    dr_fname = join_paths(data_dir, '10-464933_2_R1.hdf5.0')
+
+    astrom_filenames = dict(
+        fistar=join_paths(data_dir, '10-464933_2_R1.fistar'),
+        trans=join_paths(data_dir, '10-464933_2_R1.fistar.trans'),
+        match=join_paths(data_dir, '10-464933_2_R1.fistar.match'),
+        catalogue=join_paths(data_dir, 'test.ucac4')
+    )
+
+    path_substitutions = dict(srcextract_version=0,
+                              catalogue_version=0,
+                              skytoframe_version=0)
+    with DataReductionFile(dr_fname, 'r+') as data_reduction:
+        data_reduction.add_hat_astrometry(astrom_filenames,
+                                          **path_substitutions)
+        data_reduction.smooth_srcextract_psf(['S', 'D', 'K'],
+                                             'O3{x, y, r, J-K}',
+                                             weights_expression='1',
+                                             error_avg='median',
+                                             rej_level=5.0,
+                                             max_rej_iter=20,
+                                             **path_substitutions)
