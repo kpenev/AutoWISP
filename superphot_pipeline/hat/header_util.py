84d4540f902b8400f22c3b38ec325c0691129ad3
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2020-03-24 23:46:14 -0500

    Forgotten file from last commit.

---
+++ superphot_pipeline/hat/header_util.py
@@ -0,0 +1,6 @@
+"""A collection of utilities for working with HAT headers."""
+
+def get_jd(header):
+    """Return the mid-exposure JD given a HAT header."""
+
+    return 2.4e6 + frame_header['JD']
