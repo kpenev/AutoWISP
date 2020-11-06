26ff6f56da5bfdc530565dd35f3053aea9353dbe
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2020-11-05 22:00:26 -0600

    Remove __init__.py from light_curves which caused circular import

--- superphot_pipeline/light_curves/__init__.py
+++
@@ -1,9 +0,0 @@
-"""A collection of utilities for creating and working with light curves."""
-
-from .lc_data_slice import LCDataSlice
-from .lc_data_io import LCDataIO
-from .apply_correction import\
-    apply_parallel_correction,\
-    apply_reconstructive_correction_transit,\
-    save_correction_statistics,\
-    load_correction_statistics
