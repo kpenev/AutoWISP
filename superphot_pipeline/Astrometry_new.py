fe445114dca7a28eb16d5bb0aafa210c64f23576
Ashkan <you@example.com>
2023-02-24 20:47:22 -0600

    zeta renamed to xi, parsing moved to global env, add config and files as arguments to astrometry func, used gnemonic_projection instead of redefine it, fix diff matrix by adding absolute

--- superphot_pipeline/Astrometry_new.py
+++ superphot_pipeline/Astrometry_new.py
@@ -4,8 +4,8 @@ Created on Sun Nov  7 12:23:59 2021
 @author: Ashkan
 
 Goal: Calculating the transformation matrix as:
-(Ra,Dec) > projection(gnemonic) > (zeta,eta) > transformation (T) > (x, y)
-(Ra, Dec): on the sky which will be transformed to zeta(or xi) and eta
+(Ra,Dec) > projection(gnemonic) > (xi,eta) > transformation (T) > (x, y)
+(Ra, Dec): on the sky which will be transformed to xi and eta
 Trans_mat: Transformation Matrix which we are looking for
 (x, y): coordinates on the frame
 
@@ -31,8 +31,6 @@ from numpy.lib.recfunctions import structured_to_unstructured
 from superphot_pipeline.processing_steps.manual_util import read_catalogue
 from superphot_pipeline import source_finder_util
 from superphot_pipeline.astrometry.map_projections import gnomonic_projection
-
-################## see line 116 ###############################
 #from superphot_pipeline.astrometry.map_projections import \
 #gnomonic_projection_inv
 
@@ -79,47 +77,6 @@ def parse_command_line():
 
     return parser.parse_args()
 
-#############################################################################
-### 1st edit map_projection (2nd line), then replace this func with: ########
-### PhotometryPipeline/superphot_pipeline/astrometry/map_projections.py #####
-#############################################################################
-def projection(sources, projected, **center):
-    """
-    Project the given sky position to a tangent plane (gnomonic projection).
-    
-    Args:
-        sources(structured array-like): The the sky position to project
-            (should have "RA" and "Dec" keys coordinates in degrees.
-        
-        projected: A numpy array with "xi" and "eta" fields to fill
-            with the projected coordinates (in degrees).
-        
-        center(dict): Should define the central `'RA'` and `'Dec'` around
-            which to project. (It is supposed that RA is in hr and Dec is
-            in degree)
-    Returns:
-        None
-    """
-    degree_to_rad = np.pi / 180.0
-    center['RA'] *= np.pi / 12.0 # bc it is in hr, not degree
-    center['Dec'] *= degree_to_rad
-    ra_diff = (sources['RA']  * degree_to_rad - center['RA'])
-    cos_ra_diff = np.cos(ra_diff)
-    cos_source_dec = np.cos(sources['Dec'] * degree_to_rad)
-    cos_center_dec = np.cos(center['Dec'])
-    sin_source_dec = np.sin(sources['Dec'] * degree_to_rad)
-    sin_center_dec = np.sin(center['Dec'])
-    denominator = (sin_center_dec * sin_source_dec + \
-              cos_center_dec * cos_source_dec * cos_ra_diff) * degree_to_rad
-    
-    projected['xi'] = (cos_source_dec * np.sin(ra_diff)) / denominator
-    projected['eta'] = (
-                cos_center_dec * sin_source_dec
-                -
-                sin_center_dec * cos_source_dec * cos_ra_diff
-                ) / denominator
-    return None
-
 ##########################################################
 ##### Add this beside map_projection in: #################
 #### PhotometryPipeline/superphot_pipeline/astrometry/ ###
@@ -137,15 +94,20 @@ def inv_projection (sources, projected, **center):
         projected: numpy array with "xi" and "eta" fields (in degrees)
         
         center(dict): Should define the central "RA" and "Dec"
-                      in hour and degree respectively, which will be
-                      converted to radian
+                      in degree
         
     Returns:
         None
     """
-    center['RA'] *= np.pi / 12.0
-    center['Dec'] *= np.pi / 180.0
-    
+    degree_to_rad = np.pi / 180.0
+    rad_to_degree = 180.0 / np.pi
+
+    center['RA'] *= degree_to_rad
+    center['Dec'] *= degree_to_rad
+
+    projected['xi'] *= degree_to_rad
+    projected['eta'] *= degree_to_rad
+
     rho = np.sqrt(projected['xi']**2 + projected['eta']**2)
     c = np.arctan(rho)
     denominator = rho*np.cos(center['Dec'])*np.cos(c) - \
@@ -157,13 +119,13 @@ def inv_projection (sources, projected, **center):
     sources['Dec'] = np.arcsin(np.cos(c)*np.sin(center['Dec']) + \
           (projected['eta']*np.sin(c)*np.cos(center['Dec']))/rho)
     
-    sources['RA'] *= 12.0/np.pi    # convert from radian to degree
-    sources['Dec'] *= 180.0/np.pi  # convert from radian to degree
+    sources['RA'] *= rad_to_degree
+    sources['Dec'] *= rad_to_degree
     
     return None
 
 
-def transformation_matrix(order, zeta, eta):
+def transformation_matrix(order, xi, eta):
     """
     Setting up the transformation matrix which includes the related orders
     of (xi, eta)
@@ -172,7 +134,7 @@ def transformation_matrix(order, zeta, eta):
     Args:
         order: order of astrometry
         
-        zeta: from projected coordinates
+        xi: from projected coordinates
         
         eta: from projected coordinates
         
@@ -183,14 +145,14 @@ def transformation_matrix(order, zeta, eta):
     m = np.ones((eta.shape[0],1))
     for i in range(1, order+1):
         for j in range(i+1):
-            m = np.block([m,zeta**(i-j)*eta**j])
+            m = np.block([m,xi**(i-j)*eta**j])
     return m
     
     
 def new_center_func(z, trans_x, trans_y, x_cent, y_cent, order):
     """
     Constructing the two non linear function to be solved for
-    new center (zeta, eta)
+    new center (xi, eta)
     
     Args:
         z:
@@ -208,7 +170,7 @@ def new_center_func(z, trans_x, trans_y, x_cent, y_cent, order):
 
     """
     
-    zetac = z[0]
+    xic = z[0]
     etac = z[1]
    
     f = np.empty((2))
@@ -219,76 +181,56 @@ def new_center_func(z, trans_x, trans_y, x_cent, y_cent, order):
     k = 1
     for i in range(1, order + 1):
         for j in range(i+1):
-            f[0] = f[0] + trans_x[k, 0]*zetac**(i-j)*etac**(j)
-            f[1] = f[1] + trans_y[k, 0]*zetac**(i-j)*etac**(j)
+            f[0] = f[0] + trans_x[k, 0]*xic**(i-j)*etac**(j)
+            f[1] = f[1] + trans_y[k, 0]*xic**(i-j)*etac**(j)
             k = k + 1
     return f
 
 
-def astrometry():
+def astrometry(xy_corr, radec_corr, xy_extracted, radec_catalog, **config):
     """
     Main function: To get the initial transformation,
     the rest is calling iteration function to iterate the steps to get
-    new transformations 
+    new transformations
     
     Args:
-        None
-         
+        xy_corr: xy_corr: x and y values of the initial correlation
+        radec_corr: RA and Dec values of the initial correlation
+        xy_extracted: x and y of the extracted sources of the frame
+        radec_catalog: RA and Dec of the catalog sources
+        config: configuration including:
+            order: order of astrometry    
+            matching_max_distance: upper bound distance in kd3
+            trans_threshold: threshold for the difference of two consecutive
+            ra_cent: RA of the center of the frame
+            dec_cent: Dec of the center of the frame
+            x_frame: length of the frame in pixel
+            y_frame: width of the frame in pixel
+
+
     Returns:
         See the returns of func. iteration
                                
     """
-    
-    cmdline_config = parse_command_line()
-    
-    order = cmdline_config.order
-    matching_max_distance = cmdline_config.matching_max_distance
-    trans_threshold = cmdline_config.trans_threshold
-    ra_cent = cmdline_config.ra_cent
-    dec_cent = cmdline_config.dec_cent
-    x_frame = cmdline_config.x_frame
-    y_frame = cmdline_config.y_frame
-    corr_file = cmdline_config.corr_file 
-    fistar_file = cmdline_config.fistar_file
-    catalog_file = cmdline_config.catalog_file
-
-    
-    
-    xy_corr = \
-    np.genfromtxt(corr_file, delimiter=",", usecols=(0,1), names=['x','y'])
-    
-    radec_corr = \
-    np.genfromtxt(corr_file, delimiter=",", usecols=(6,7), names=['RA','Dec'])
-    
-
-    xy_extracted = np.genfromtxt(
-         fistar_file,names=source_finder_util.get_srcextract_columns('fistar'),
-         dtype=None, deletechars=''
-    )
 
 #############################################################################
 ##### use:
 #from superphot_pipeline.processing_steps.manual_util import read_catalogue #
 #############################################################################
 
-    radec_catalog = np.genfromtxt(
-             catalog_file, skip_header=1,usecols=(1,2), names=['RA','Dec'])
     
     projected = \
     np.empty(radec_corr.shape[0], dtype=[('xi', float), ('eta', float)])
-    
-    radec_cent = {"RA":ra_cent, "Dec":dec_cent}
-    
-    projection(radec_corr, projected, **radec_cent)
 
-    zeta = projected['xi']
-    zeta = zeta[np.newaxis].T  # convert shape from (n,) to (n, 1)
+    radec_cent = {"RA":config['ra_cent'], "Dec":config['dec_cent']}
     
-    eta = projected['eta']
-    eta = eta[np.newaxis].T  # convert shape from (n,) to (n, 1)
-     
-    # Matrix (m) containing zeta, eta. Used to find Transformation(t)
-    mt = transformation_matrix(order, zeta, eta)
+    gnomonic_projection(radec_corr, projected, **radec_cent)
+
+    xi = projected['xi'][np.newaxis].T
+    
+    eta = projected['eta'][np.newaxis].T
+
+    mt = transformation_matrix(order, xi, eta)
 
 
     trans_x, resid_x, rank_x, sigma_x = linalg.lstsq(mt, xy_corr['x'])
@@ -303,12 +245,13 @@ def astrometry():
     logging.debug(trans_y)
     
 
-    parameters = {'order':order,'matching_max_distance':matching_max_distance,
-                  'trans_threshold':trans_threshold,'trans_x':trans_x,
-                  'trans_y':trans_y, 'ra_cent':ra_cent, 'dec_cent':dec_cent,
-                  'x_frame':x_frame, 'y_frame':y_frame, 'xy_corr':xy_corr,
-                  'radec_corr':radec_corr, 'xy_extracted':xy_extracted,
-                  'radec_catalog':radec_catalog}
+    files = {'trans_x':trans_x, 'trans_y':trans_y,
+             'xy_corr':xy_corr, 'radec_corr':radec_corr,
+             'xy_extracted':xy_extracted,
+             'radec_catalog':radec_catalog
+    }
+
+    parameters = {**config,**files}
     
     return(iteration(parameters))
 
@@ -384,18 +327,17 @@ def iteration(params):
         projected = np.empty(
         radec_catalog.shape[0], dtype=[('xi', float), ('eta', float)]
         )
-        projection(radec_catalog, projected, **radec_cent)
+        gnomonic_projection(radec_catalog, projected, **radec_cent)
 
         
-        zeta = projected['xi']
-        zeta = zeta.reshape(len(zeta),1)  #convert shape from (n,) to (n, 1)    
+        xi = projected['xi']
+        xi = xi.reshape(len(xi),1)  #convert shape from (n,) to (n, 1)    
         eta = projected['eta']
         eta = eta.reshape(len(eta),1)   #convert shape from (n,) to (n, 1)
     
-        mxy = transformation_matrix(order, zeta, eta)
+        mxy = transformation_matrix(order, xi, eta)
         x_transformed = mxy @ trans_x
         y_transformed = mxy @ trans_y
-        # we should use: np.flatnonzero(x>2)
 
         xy_transformed = np.block([x_transformed, y_transformed])
 
@@ -403,35 +345,19 @@ def iteration(params):
         d, ix = kdtree.query(
             xy_transformed, distance_upper_bound = matching_max_distance
         )
-        #####################################################################
-        ### Should we let the user choose upper bound distance? #############
-        #####################################################################
-        result, count = np.unique(ix, return_counts = True)
+
         
-        # Here we find those catalog sources that are matched with a common
-        # frame source. We keep only the nearest source:
+        result, count = np.unique(ix, return_counts = True)
+
+        # Finding catalog sources that are matched with a common
+        # frame source and remove all of them:
         for multi_match_i in result[count > 1][:-1]:
             bad_match = (ix == multi_match_i)
-            # This is the desired index that should be kept:
-            found_index = np.argwhere(bad_match)[np.argmin(d[bad_match])][0]
-            bad_match[found_index] = False
-            good_match = np.invert(bad_match)
             d[bad_match] = np.inf
             ix[bad_match] = result[-1]
-        ######################################################################
-        ### IMP: We should try to see if there is a better way to do this ####
-        ### removing of multiple matched sources. Or not, as this part of ####
-        ### the astromy process is the least time consuming part #############
-        ######################################################################
 
-
-        n_matched = 0
-        res_sum = 0
-
-        for i in d:
-            if not np.isinf(i):
-                n_matched = n_matched + 1
-                res_sum = res_sum + i**2
+        n_matched = sum(1 for i in d if i != np.inf)
+        res_sum = sum(i**2 for i in d if i != np.inf)
 
         logging.debug("# of matched: {}".format(n_matched))
         matched_sources = \
@@ -443,28 +369,29 @@ def iteration(params):
         k = -1
         
         for i in range(ix.size):
-            k = k + 1
+            k += 1
             if not np.isinf(d[i]):
                 matched_sources['RA'][j] = radec_catalog['RA'][k]
                 matched_sources['Dec'][j] = radec_catalog['Dec'][k]
                 matched_sources['x'][j] = xy_extracted[ix[i], 0]
                 matched_sources['y'][j] = xy_extracted[ix[i], 1]
                 j = j + 1
-        zGuess = np.array([np.mean(zeta), np.mean(eta)])
+
+        zGuess = np.array([np.mean(xi), np.mean(eta)])
         z = fsolve(
         new_center_func, zGuess, args=(trans_x, trans_y, x_cent, y_cent, order)
         )
         
-        # since when we derived T, zeta and eta were in degrees.
-        zetaeta_cent = np.empty(1, dtype=[('xi', float), ('eta', float)])
-        zetaeta_cent['xi'] = z[0]*np.pi/180
-        zetaeta_cent['eta'] = z[1]*np.pi/180
+        # since when we derived T, xi and eta were in degrees.
+        xieta_cent = np.empty(1, dtype=[('xi', float), ('eta', float)])
+        xieta_cent['xi'] = z[0]*np.pi/180
+        xieta_cent['eta'] = z[1]*np.pi/180
 
-        # With those (zeta, eta) we use the inverse projection to
+        # With those (xi, eta) we use the inverse projection to
         # find new (RAC, DECC)
         # radec_cent = {"RA":ra_cent, "Dec":dec_cent} < we already have this
         source = np.empty(1, dtype=[('RA', float), ('Dec', float)])
-        inv_projection(source, zetaeta_cent, **radec_cent)
+        inv_projection(source, xieta_cent, **radec_cent)
         # Replace ra_cent, dec_cent with new values in unprojected
         cent_new = {
         'RA':source['RA'][0], 'Dec':source['Dec'][0]
@@ -474,21 +401,21 @@ def iteration(params):
         matched_sources.shape[0], dtype=[('xi', float), ('eta', float)]
         )
 
-        projection(matched_sources, projected_new, **cent_new)
+        gnomonic_projection(matched_sources, projected_new, **cent_new)
         
         matched_sources = structured_to_unstructured(matched_sources)
         projected_new = structured_to_unstructured(projected_new)
         x_matched_sources = matched_sources[:,2:3]
         y_matched_sources = matched_sources[:,3:4]
-        zeta = projected_new[:,0:1]
+        xi = projected_new[:,0:1]
         eta = projected_new[:,1:2]
 
-        mt = transformation_matrix(order, zeta, eta)
+        mt = transformation_matrix(order, xi, eta)
 
         trans_x_new, resid, rank, sigma = linalg.lstsq(mt, x_matched_sources)
         trans_y_new, resid, rank, sigma = linalg.lstsq(mt, y_matched_sources)
 
-        logging.debug('\n new Transformation matrix for X components:')
+        logging.debug('\n newTransformation matrix for X components:')
         logging.debug(trans_x_new)
         logging.debug('\n new Transformation matrix for Y components:')
         logging.debug(trans_y_new)
@@ -498,8 +425,9 @@ def iteration(params):
         ### 2. Should we let the user to choose this? ############
         ##########################################################
 
-        diff = np.block([trans_x_new - trans_x, trans_y_new - trans_y])
-
+        diff = np.block([np.absolute(trans_x_new - trans_x),
+                         np.absolute(trans_y_new - trans_y)])
+        
         if np.count_nonzero(diff > trans_threshold) == 0:
             # Exclude the sources that are not within the frame:
             in_frame = np.logical_and(
@@ -526,13 +454,51 @@ def iteration(params):
                     )  
 
             print('finished!, # of iteration =', counter)
-            # outputs={} put all here
+
             return n_matched, n_extracted, n_catalog, res_sum
 
 
 if __name__ == '__main__':
 
-    n_matched, n_extracted, n_catalog, res_sum = astrometry()
+    cmdline_config = parse_command_line()
+    
+    order = cmdline_config.order
+    matching_max_distance = cmdline_config.matching_max_distance
+    trans_threshold = cmdline_config.trans_threshold
+    ra_cent = cmdline_config.ra_cent
+    dec_cent = cmdline_config.dec_cent
+    x_frame = cmdline_config.x_frame
+    y_frame = cmdline_config.y_frame
+    corr_file = cmdline_config.corr_file 
+    fistar_file = cmdline_config.fistar_file
+    catalog_file = cmdline_config.catalog_file
+
+    config = {
+            'order':order,'matching_max_distance':matching_max_distance,
+            'trans_threshold':trans_threshold, 'ra_cent':ra_cent,
+            'dec_cent':dec_cent, 'x_frame':x_frame, 'y_frame':y_frame, 
+    }
+
+    xy_corr = np.genfromtxt(
+          corr_file, delimiter=",", usecols=(0,1), names=['x','y']
+    )
+    
+    radec_corr = np.genfromtxt(
+          corr_file, delimiter=",", usecols=(6,7), names=['RA','Dec']
+    )
+    
+    xy_extracted = np.genfromtxt(
+         fistar_file,names=source_finder_util.get_srcextract_columns('fistar'),
+         dtype=None, deletechars=''
+    )
+
+    radec_catalog = np.genfromtxt(
+         catalog_file, skip_header=1,usecols=(1,2), names=['RA','Dec']
+    )
+
+    n_matched, n_extracted, n_catalog, res_sum = astrometry(
+                 xy_corr, radec_corr, xy_extracted, radec_catalog, **config
+    )
     
     logging.debug("# of catalog sources: {}".format(n_catalog))
     logging.debug("# of extracted: {}".format(n_extracted))
