19e6e915e039fdc5678ee61ced20d90e672fbcbd
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2023-09-11 19:27:58 -0500

    Implement more convenient interface for querying Gaia for astrometry

--- superphot_pipeline/astrometry/catalog.py
+++ superphot_pipeline/astrometry/catalog.py
@@ -5,7 +5,7 @@ from astropy import units
 from astroquery.gaia import GaiaClass, conf
 
 class SuperPhotGaia(GaiaClass):
-    """Extend query_object with condition and sorting."""
+    """Extend queries with condition and sorting."""
 
     def query_object_filtered(self,
                               *,
@@ -17,7 +17,7 @@ class SuperPhotGaia(GaiaClass):
                               order_by,
                               epoch=None,
                               columns=None,
-                              order_dir='DESC',
+                              order_dir='ASC',
                               verbose=False):
         """
         Get GAIA sources within a box satisfying given condition (ADQL).
@@ -41,6 +41,10 @@ class SuperPhotGaia(GaiaClass):
 
             order_dir(str):    Should the order be ascending (``'ASC'``) or
                 descending (``'DESC'``).
+
+        Returns:
+            astropy Table:
+                The result of the query.
         """
 
         def get_result(query, add_propagated):
@@ -79,7 +83,7 @@ class SuperPhotGaia(GaiaClass):
                 except ValueError:
                     pass
 
-            columns = ','.join(map(str, columns))
+            columns = ', '.join(map(str, columns))
 
         if epoch is not None:
             epoch = epoch.to_value(units.yr)
@@ -123,19 +127,72 @@ class SuperPhotGaia(GaiaClass):
             add_propagated
         )
 
+
+    def query_brightness_limited(self,
+                                 *,
+                                 magnitude_expression,
+                                 magnitude_limit,
+                                 **query_kwargs):
+        """
+        Get sources within a box and a range of magnitudes.
+
+        Args:
+            magnitude_expression:    Expression for the relevant magnitude
+                involving Gaia columns.
+
+            magnitude_limit:    Either upper limit or lower and upper limit on
+                the magnitude defined by ``magnitude_expression``.
+
+            **query_kwargs:    Arguments passed directly to
+                `query_object_filtered()`.
+
+        Returns:
+            astropy Table:
+                The result of the query.
+        """
+
+        if query_kwargs.get('columns', False):
+            query_kwargs['columns'].append(
+                f'({magnitude_expression}) AS magnitude'
+            )
+        else:
+            query_kwargs['columns'] = [f'({magnitude_expression}) AS magnitude',
+                                       '*']
+
+        if 'order_by' not in query_kwargs:
+            query_kwargs['order_by'] = 'magnitude'
+            query_kwargs['order_dir'] = 'ASC'
+
+        try:
+            min_mag, max_mag = magnitude_limit
+            condition = (f'({magnitude_expression}) > {min_mag} AND '
+                         f'({magnitude_expression}) < {max_mag}')
+        except TypeError:
+            condition = f'{magnitude_expression} < {magnitude_limit}'
+
+        if 'condition' in query_kwargs:
+            query_kwargs['condition'] = (
+                f'({query_kwargs["condition"]}) AND ({condition})'
+            )
+        else:
+            query_kwargs['condition'] = condition
+
+        return self.query_object_filtered(**query_kwargs)
+
 gaia = SuperPhotGaia()
 
 if __name__ == '__main__':
-    gaia.query_object_filtered(
+    gaia.query_brightness_limited(
         ra=118.0 * units.deg,
         dec=2.6 * units.deg,
         width=1.8 * units.deg,
         height=1.3 * units.deg,
         epoch=2023.5 * units.yr,
-        condition='phot_g_mean_mag < 11',
-        order_by='phot_g_mean_mag',
+        magnitude_expression='phot_g_mean_mag - 5',
+        magnitude_limit=6,
         verbose=True
     ).write(
         'test_gaia.txt',
         format='ascii.fixed_width',
+        overwrite=True
     )
