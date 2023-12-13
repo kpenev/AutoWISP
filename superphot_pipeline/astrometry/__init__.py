b65629983591b1532d64fb6d8bbdcea9e2e4a46b
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2023-12-13 11:25:14 -0600

    Completely untested draft of automated astrometry catalog generation

--- superphot_pipeline/astrometry/__init__.py
+++ superphot_pipeline/astrometry/__init__.py
@@ -5,6 +5,7 @@ from superphot_pipeline.astrometry.anmatch_transformation import\
     AnmatchTransformation
 from superphot_pipeline.astrometry.astrometry import \
     estimate_transformation,\
-    refine_transformation
+    refine_transformation,\
+    find_ra_dec_center
 
 __all__ = ['Transformation', 'AnmatchTransformation']
