43b8a0aef86d949d0879e3b83ba54ee603bdf30f
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2024-07-05 17:05:11 -0500

    Begin isolating basic FITS image view

--- browser_interface/processing/select_photref_views.py
+++ browser_interface/processing/select_photref_views.py
@@ -1,20 +1,15 @@
 """Implement the view for selecting single photometric reference."""
 
-from io import BytesIO, StringIO
-from base64 import b64encode
+from io import StringIO
+import json
 
-import numpy
-from PIL import Image
 #from PIL.ImageTransform import AffineTransform
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import matplotlib
-from matplotlib import colors, pyplot
+from matplotlib import pyplot
 from sqlalchemy import select
 import pandas
-
-from astropy.io import fits
-from astropy.visualization import ZScaleInterval
 
 from autowisp.database.processing import\
     ProcessingManager,\
@@ -32,42 +27,7 @@ from autowisp.database.data_model import\
     ConditionExpression,\
     Step
 #pylint: enable=no-name-in-module
-
-
-def _log_transform(pixel_values, parameter=1000.0):
-    """Perform the same log-transform as DS9."""
-
-    return numpy.log(parameter * pixel_values + 1) / numpy.log(parameter)
-
-
-def _pow_transform(pixel_values, parameter=1000.0):
-    """Perform the same pow transfom as DS9."""
-
-    return (numpy.power(parameter, pixel_values) - 1.0) / parameter
-
-
-def _sqrt_transform(pixel_values):
-    """Use square root of the pixel values as intensity."""
-
-    return numpy.sqrt(pixel_values)
-
-
-def _square_transform(pixel_values):
-    """Use the square of the pixel values as intensity."""
-
-    return numpy.square(pixel_values)
-
-
-def _asinh_transform(pixel_values):
-    """The asinh transform of DS9."""
-
-    return numpy.arcsinh(10.0 * pixel_values) / 3.0
-
-
-def _sinh_transform(pixel_values):
-    """The sinh transform of DS9."""
-
-    return numpy.sinh(3.0 * pixel_values) / 10.0
+from autowisp.bui_util import encode_fits
 
 
 def _get_missing_photref(request):
@@ -224,116 +184,91 @@ def _create_merit_histograms(merit_data, image_index):
     return result
 
 
+def update_fits_display(request):
+    """Updatet the displayed FITS image per user interaction."""
+
+    assert request.method == 'POST'
+    request_data = json.loads(request.body.decode())
+    print(f'Request data: {request_data!r}')
+    change = request_data.pop('change')
+    if change == 'next image':
+        request.session['fits_display']['image_index'] += 1
+    elif change == 'previous image':
+        request.session['fits_display']['image_index'] -= 1
+    else:
+        assert len(change) == 1
+        for param, value in change.items():
+            request.session['fits_display'][param] = value
+    request.session['view_config'] = json.dumps(request_data)
+    print('Request session:\n\t'
+          +
+          '\n\t'.join(f'{k!r}: {v!r}' for k, v in request.session.items()))
+    request.session.modified = True
+
+    return JsonResponse({'ok': True})
+
+
 def select_photref_image(request,
                          *,
                          target_index,
-                         image_index=0,
-                         values_range='zscale',
-                         values_transform=None,
                          recalculate=False):
     """Display the interface for reviewing canditate reference frames."""
 
+    assert request.method == 'GET'
     print('Image view with request: ' + repr(request))
+    if 'fits_display' not in request.session:
+        request.session['fits_display'] = {
+            'image_index': 0,
+            'range': 'zscale',
+            'transform': None,
+        }
+    image_index = request.session['fits_display']['image_index']
     if recalculate:
         print('Deleting merit info')
         del request.session['merit_info']
-#        request.session.modified = True
-#        return redirect('/processing/select_photref_target',
-#                        target_index=target_index,
-#                        image_index=image_index,
-#                        values_range=values_range,
-#                        values_transform=values_transform)
     _get_merit_data(request, target_index)
     print('Merit info keys: ' + repr(request.session['merit_info'].keys()))
 
     merit_data = pandas.read_json(
         request.session['merit_info'][str(target_index)]
     )
-    if request.method == 'POST':
-        request.session['view_config'] = request.body.decode()
-        print('Redirecting with new range: ' + repr(values_range))
-        return JsonResponse({'ok': True})
-    print('Rendering with values range: ' + repr(values_range))
-    print('Merit data:\n' + repr(merit_data))
-    print('View config:\n' + repr(request.session.get('view_config')))
-    png_stream = BytesIO()
-    with fits.open(
-        request.session[
-            'need_photref'
-        ][
-            'master_values'
-        ][
-            target_index
-        ][
-            2
-        ][
-            #False positive
-            #pylint:disable=no-member
-            merit_data.index[image_index]
-            #pylint:enable=no-member
-        ][
-            0
-        ],
-        'readonly'
-    ) as frame:
-        if values_range == 'zscale':
-            limits = ZScaleInterval().get_limits(frame[1].data)
-        elif values_range == 'minmax':
-            limits = frame[1].data.min(), frame[1].data.max()
-        else:
-            limits = tuple(int(lim.strip())
-                           for lim in values_range.split(','))
-        pixel_values = colors.Normalize(
-            *limits,
-            True
-        )(frame[1].data)
-        if values_transform is not None and values_transform != 'None':
-            transform_args = values_transform.split('-')
-            transform = globals()['_' + transform_args.pop(0) + '_transform']
-            transform_args = [float(arg) for arg in transform_args]
-            pixel_values = transform(pixel_values, *transform_args)
-        scaled_pixels = (
-            pixel_values
-            *
-            255
-        ).astype('uint8')
-        image = Image.fromarray(scaled_pixels)
-        #apply_zoom = AffineTransform((1.0/zoom, 0, 0, 0, 1.0/zoom, 0.0))
-        #image.transform(
-        #    size=(int(image.size[0] * zoom), int(image.size[1] * zoom)),
-        #    method=apply_zoom
-        #).save(
-        #    png_stream,
-        #    'png'
-        #)
-        image.save(png_stream, 'png')
-
-    print('Rendering')
+    fits_fname = request.session[
+        'need_photref'
+    ][
+        'master_values'
+    ][
+        target_index
+    ][
+        2
+    ][
+        #False positive
+        #pylint:disable=no-member
+        merit_data.index[image_index]
+        #pylint:enable=no-member
+    ][
+        0
+    ]
+    context = {
+        'target_index': target_index,
+        #False positive
+        #pylint: disable=no-member
+        'last_image': image_index == merit_data.shape[0] - 1,
+        #pylint: enable=no-member
+        'histograms': _create_merit_histograms(merit_data, image_index),
+        'view_config': request.session.get('view_config', 'undefined')
+    }
+    context.update(request.session['fits_display'])
+    context.update(
+        encode_fits(
+            fits_fname,
+            request.session['fits_display']['range'],
+            request.session['fits_display']['transform']
+        )
+    )
     return render(
         request,
         'processing/select_photref_image.html',
-        {
-            'target_index': target_index,
-            'image_index': image_index,
-            #False positive
-            #pylint: disable=no-member
-            'last_image': image_index < merit_data.shape[0] - 1,
-            #pylint: enable=no-member
-            'image': b64encode(png_stream.getvalue()).decode('utf-8'),
-            'histograms': _create_merit_histograms(merit_data, image_index),
-            'range': values_range,
-            'transform': values_transform,
-            'transform_list': [
-                entry[1:].split('_', 1)[0]
-                for entry in globals()
-                if(
-                    entry[0] == '_'
-                    and
-                    entry.endswith('_transform')
-                )
-            ],
-            'view_config': request.session.get('view_config', 'undefined')
-        }
+        context
     )
 
 
@@ -395,15 +330,15 @@ def record_photref_selection(request, target_index, image_index):
         target_index
     ]
     del request.session['merit_info'][str(target_index)]
-    for target_index in range(target_index + 1,
-                              len(request.session[
-                                  'need_photref'
-                              ][
-                                  'master_values'
-                              ])):
-        if str(target_index) in request.session['merit_info']:
-            request.session['merit_info'][str(target_index - 1)] =\
-                request.session['merit_info'].pop(str(target_index))
+    for shift_index in range(target_index + 1,
+                             len(request.session[
+                                 'need_photref'
+                             ][
+                                 'master_values'
+                             ])):
+        if str(shift_index) in request.session['merit_info']:
+            request.session['merit_info'][str(shift_index - 1)] =\
+                request.session['merit_info'].pop(str(shift_index))
 
     request.session.modified = True
 
--- browser_interface/processing/static/processing/js/select.photref.js
+++ browser_interface/processing/static/processing/js/select.photref.js
@@ -1,84 +1,3 @@
-//Place the main image relative to its parent per its posX and posY attributes
-function placeImage()
-{
-    let boundingRect = document.getElementsByClassName(
-        "main-parent"
-    )[0].getBoundingClientRect();
-
-    image.style.left = (
-        image.posX 
-        + 
-        Math.round((boundingRect.width - image.width) / 2)
-    ) + "px";
-    image.style.top = (
-        image.posY 
-        + 
-        Math.round((boundingRect.height - image.height) / 2)
-    ) + "px";
-}
-
-//Change the zoom level of the main image.
-function adjustZoom(event)
-{
-    event.preventDefault();
-    let image = document.getElementById("main-image");
-    let parentWidth = document.getElementsByClassName("main-parent")[0].getBoundingClientRect().width;
-    let step = Math.round(image.width / 100);
-    newWidth = Math.max(
-        100,
-        image.width + event.deltaY * step
-    );
-    newWidth = Math.min(newWidth, 100 * image.naturalWidth)
-    if ( image.width > image.naturalWidth && newWidth < image.naturalWidth ) {
-        newWidth = image.naturalWidth;
-    } else if ( image.width > parentWidth 
-                && 
-                newWidth < parentWidth ) {
-        newWidth = parentWidth;
-    } 
-
-    let scale = newWidth / image.width
-    image.posX = image.posX * scale;
-    image.posY = image.posY * scale;
-    image.width = newWidth;
-    image.height = Math.round(image.naturalHeight 
-                              * 
-                              image.width 
-                              / 
-                              image.naturalWidth);
-    placeImage();
-}
-
-//Change the displayed portion of the main image in response to dragging.
-function pan(event)
-{
-    event.preventDefault();
-    let shiftX = event.clientX - pan.startX;
-    let shiftY = event.clientY - pan.startY;
-
-    image.posX = pan.imageStartX + shiftX;
-    image.posY = pan.imageStartY + shiftY;
-    placeImage();
-}
-
-//Prepare to respond to the user dragging the main image. 
-function panStart(event)
-{
-    event.preventDefault();
-    pan.startX = event.clientX;
-    pan.startY = event.clientY;
-    pan.imageStartX = image.posX;
-    pan.imageStartY = image.posY;
-    image.addEventListener("mousemove", pan);
-}
-
-//The user has released the main image after dragging it.
-function panStop(event)
-{
-    event.preventDefault();
-    image.removeEventListener("mousemove", pan); 
-}
-
 //Change the displayed histogrames up by one.
 function histScrollUp(event)
 {
@@ -202,33 +121,18 @@ function resizeHistEnd(event)
 function initView(viewConfig)
 {
     if ( viewConfig === undefined ) {
+        initFITS();
         viewConfig = {
-            "image": {
-                "posX": 0, 
-                "posY": 0, 
-                "width": image.width,
-                "height": image.height
-            },
+            "image": getFITSConfig(),
             "histograms": {
                 "firstVisible": 0,
                 "width": document.getElementById("side-bar").style.width,
                 "order": [...Array(histParent.children.length).keys()]
             }
         };
-
+    } else {
+        initFITS(viewConfig["image"]);
     }
-    image.addEventListener("wheel", adjustZoom);
-    image.addEventListener("mousedown", panStart);
-    image.addEventListener("mouseup", panStop);
-    image.posX = viewConfig.image.posX;
-    image.posY = viewConfig.image.posY;
-    image.width = viewConfig.image.width;
-    image.height = Math.round(image.naturalHeight 
-                              * 
-                              image.width 
-                              / 
-                              image.naturalWidth);
-    placeImage();
 
     for( let i = 0; i < histParent.children.length; i++) {
         histParent.children[i].addEventListener("mousedown", histDragStart);
@@ -268,15 +172,11 @@ function initView(viewConfig)
 }
 
 //Submit the currently configured view when changing scale, range or image
-async function updateView(updateURL)
+async function updateView(change)
 {
     viewConfig = {
-        "image": {
-            "posX": image.posX, 
-            "posY": image.posY, 
-            "width": image.width,
-            "height": image.height
-        },
+        "change": change,
+        "image": getFITSConfig(),
         "histograms": {
             "firstVisible": histParent.firstVisible,
             "width": document.getElementById("side-bar").style.width,
@@ -287,12 +187,11 @@ async function updateView(updateURL)
     for( let i = 0; i < histParent.children.length; i++) {
         viewConfig.histograms.order.push(histParent.children[i].origPosition);
     }
-    postJson(updateURL, viewConfig).then(
+    postJson(updateView.URL, viewConfig).then(
         function() {
-            document.location = updateURL;
+            location.reload();
         }
     );
 }
 
-var image = document.getElementById("main-image")
 var histParent = document.getElementById("hist-parent");
