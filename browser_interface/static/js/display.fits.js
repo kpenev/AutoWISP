c3d3b10ebac12da152c59bc85a46f2e19d562dc2
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2024-07-10 15:56:48 -0500

    Further along in implementing regions in FITS BUI views

--- browser_interface/static/js/display.fits.js
+++ browser_interface/static/js/display.fits.js
@@ -1,9 +1,11 @@
+var svgNS = "http://www.w3.org/2000/svg";
+
 //Place the main image relative to its parent per its posX and posY attributes
 function placeImage()
 {
-    let boundingRect = document.getElementsByClassName(
-        "main-parent"
-    )[0].getBoundingClientRect();
+    let boundingRect = document.getElementById(
+        "img-parent"
+    ).getBoundingClientRect();
 
     image.style.left = (
         image.posX 
@@ -15,6 +17,24 @@ function placeImage()
         + 
         Math.round((boundingRect.height - image.height) / 2)
     ) + "px";
+
+    let regionsElement = document.getElementById("regions");
+    if ( regionsElement != null ) {
+        regionsElement.style.left = (
+            boundingRect.left 
+            +
+            image.posX
+            +
+            Math.round((boundingRect.width - image.width) / 2)
+        ) + "px";
+        regionsElement.style.top = (
+            boundingRect.top 
+            + 
+            image.posY
+            +
+            Math.round((boundingRect.height - image.height) / 2)
+        ) + "px";
+    }
 }
 
 //Change the zoom level of the main image.
@@ -22,7 +42,7 @@ function adjustZoom(event)
 {
     event.preventDefault();
     let image = document.getElementById("main-image");
-    let parentWidth = document.getElementsByClassName("main-parent")[0].getBoundingClientRect().width;
+    let parentWidth = document.getElementById("img-parent").getBoundingClientRect().width;
     let step = Math.round(image.width / 100);
     newWidth = Math.max(
         100,
@@ -47,6 +67,12 @@ function adjustZoom(event)
                               / 
                               image.naturalWidth);
     placeImage();
+
+    let regionsElement = document.getElementById("regions");
+    if ( regionsElement != null ) {
+        regionsElement.setAttribute("width", image.width);
+        regionsElement.setAttribute("height", image.height);
+    }
 }
 
 //Change the displayed portion of the main image in response to dragging.
@@ -127,7 +153,87 @@ function initView(viewConfig)
     }
 }
 
-//Submit the currently configured view when changing scale, range or image
+//Transform given regions from pixel to fractional coordinates.
+function scaleRegions(regions)
+{
+    let x_scale = image.naturalWidth;
+    let y_scale = image.naturalHeight;
+    let r_scale = max(x_scale, y_scale);
+    const result = regions.slice();
+    for( let i = 0; i < regions.length; i++) {
+        for(let param in regions[i]) {
+            if ( param == "x" || param == "width" ) {
+                result[i][param] = regions[i][param] / x_scale;
+            } else if ( param == "x" || param == "width" ) {
+                result[i][param] = regions[i][param] / y_scale;
+            } else if ( param == "r" ) {
+                result[i][param] = regions[i][param] / r_scale;
+            }
+        }
+    }
+    return result;
+}
+
+function getRegionsElement()
+{
+    let regionsElement = document.getElementById("regions");
+    if ( regionsElement != null ) {
+        return regionsElement;
+    }
+    let mainParent = document.getElementById("img-parent");
+    let boundingRect = mainParent.getBoundingClientRect();
+    if ( regionsElement == null ) {
+        alert("creating new regions element");
+        regionsElement = document.createElementNS(svgNS, 'svg');
+        regionsElement.id = "regions"
+        regionsElement.style.position = "absolute";
+        regionsElement.setAttribute("width", image.width);
+        regionsElement.setAttribute("height", image.height);
+        regionsElement.style.background = '#7777'
+        mainParent.appendChild(regionsElement);
+        placeImage();
+    }
+    return regionsElement;
+}
+
+//Similar to regions in DS9, but lower left corver of lower left pixel is (0,0)
+function addRegions(
+                    //The regions to draw. The format is JSON:
+                    //{
+                    //  "shape": <the name of the shape to mark with>
+                    //  "x": <shape center x coordinate>
+                    //  "y": <shape center y coordinate>
+                    //  ... <other shape parameters e.g. width, height> ...
+                    //  "color": <color>    
+                    //  "linewidth": <linewidth to draw shape with>
+                    //}
+                    //Color and linewidth are optional and default to "green" 
+                    //and 1
+                    //
+                    //Supported regions are: rect, circle, ellipse, x, +
+                    regions, 
+
+                    //Should be either "px" (coordinates are specified in
+                    //pixels) or "frac" (coordinates are specified as fraction
+                    //of the image width for x and height for y). Default is
+                    //"frac"
+                    units)
+{
+    if ( units == "px" ) {
+        addRegions(scaleRegions(regions));
+    }
+    let regionsElement = getRegionsElement();
+    let circ = document.createElementNS(svgNS, 'circle');
+    circ.setAttribute("cx", "50%");
+    circ.setAttribute("cy", "50%");
+    circ.setAttribute("r", "10%");
+    circ.setAttribute("stroke-width", "5");
+    circ.setAttribute("fill", "yellow");
+    regionsElement.appendChild(circ);
+}
+
+//Submit the currently c{
+//}onfigured view when changing scale, range or image
 async function updateView(change)
 {
     viewConfig = {
