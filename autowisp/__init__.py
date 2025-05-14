af331e3e390cca09f6e701fddd71146c266f8286
Kaloyan Penev <kpenev@gmail.com>
2025-05-14 11:07:01 -0500

    Screwed up order of imports in previous commit

--- autowisp/__init__.py
+++ autowisp/__init__.py
@@ -1,7 +1,5 @@
 """A general purpose AstroWISP based photometry pipeline."""
 
-from autowisp.evaluator import Evaluator
-
 try:
     from autowisp.processor import Processor
     from autowisp.data_reduction.data_reduction_file import\
@@ -11,5 +9,9 @@ try:
     from autowisp.light_curves.tfa_correction import TFACorrection
     from autowisp.source_finder import SourceFinder
     from autowisp.piecewise_bicubic_psf_map import PiecewiseBicubicPSFMap
+    #pylint: disable=bare-except
 except:
+    #pylint: enable=bare-except
     pass
+
+from autowisp.evaluator import Evaluator
