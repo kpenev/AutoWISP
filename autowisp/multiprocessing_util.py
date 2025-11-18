e0f4bcc773143fd9ff199c9a7a90f2322700e050
Kaloyan Penev <kpenev@gmail.com>
2025-11-18 12:22:52 -0600

    Unit test bugfix + disable astrometry (astrometry.net not working)

--- autowisp/multiprocessing_util.py
+++ autowisp/multiprocessing_util.py
@@ -8,7 +8,6 @@ import re
 from glob import glob
 import inspect
 import sys
-import multiprocessing
 
 import platformdirs
 
@@ -152,25 +151,28 @@ def setup_process_map(config):
         ):
             config[param] = value
 
-    with open(
-        os.path.join(
-            platformdirs.user_data_dir("autowisp"), "setup_process.outerr"
-        ),
-        "a",
-        encoding="utf-8",
-    ) as info_file:
-        info_file.write(
-            f"Setting up process with project home {config['project_home']} and configuration:\n\t"
-            + "\n\t".join(
-                f"{key!r}: {value!r}" for key, value in config.items()
+    app_data_dir = platformdirs.user_data_dir("autowisp")
+    if os.path.exists(app_data_dir): # allow running on GitHub Actions
+        with open(
+            os.path.join(app_data_dir, "setup_process.outerr"),
+            "a",
+            encoding="utf-8",
+        ) as info_file:
+            info_file.write(
+                f"Setting up process with project home {config['project_home']}"
+                "and configuration:\n\t"
+                + "\n\t".join(
+                    f"{key!r}: {value!r}" for key, value in config.items()
+                )
+                + "\n"
             )
-            +"\n"
-        )
-        logging_fname, std_out_err_fname = get_log_outerr_filenames(**config)
-        info_file.write(
-            f"Logging to {logging_fname!r}, "
-            f"stdout/stderr to {std_out_err_fname!r}\n"
-        )
+            logging_fname, std_out_err_fname = get_log_outerr_filenames(
+                **config
+            )
+            info_file.write(
+                f"Logging to {logging_fname!r}, "
+                f"stdout/stderr to {std_out_err_fname!r}\n"
+            )
 
     if std_out_err_fname is not None:
         sys.stdout.flush()
@@ -204,7 +206,7 @@ def setup_process_map(config):
 
     logging.info("Starting process with configuration: %s", repr(config))
 
-    set_project_home(config['project_home'])
+    set_project_home(config["project_home"])
     if "data_reduction_fname" in config:
         DataReductionFile.fname_template = config["data_reduction_fname"]
 
